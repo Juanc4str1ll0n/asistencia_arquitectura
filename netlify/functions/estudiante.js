676fd1cb8e0247a0c0882df58cc3c38cd5bdc57c
Juanc4str1ll0n <juancastrillon868@gmail.com>
2025-05-20 20:06:17 -0500

    cambios en routes controllers y functions

---
+++ netlify/functions/estudiante.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var cors = require("cors");
+var serverless = require ('serverless-http');
+var port = process.env.PORT || 5000;
+var app = express();
+var estudiantesroutes = require("../../Backend/routes/estudiantesroutes.js");
+app.use(express.json());
+app.use(cors());
+
+var router = express.Router();
+router.use ("/estudiantes",estudiantesroutes);
+var handler = app.use ('/.netlify/functions',router);
+exports.handler = serverless (app);
+
+//hola
