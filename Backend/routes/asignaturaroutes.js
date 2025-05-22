505b34a82b80293e326836637a817685869860f1
Juanc4str1ll0n <juancastrillon868@gmail.com>
2025-05-22 10:41:53 -0500

    Prueba netlify

--- Backend/routes/asignaturaroutes.js
+++ Backend/routes/asignaturaroutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const asignaturaController = require("../controllers/asignaturacontroller");
+const asignaturaController = require("../controllers/asignaturacontroller.js");
 
 router.get("/", asignaturaController.consultar);
 router.post("/", asignaturaController.ingresar);
