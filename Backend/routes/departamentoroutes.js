d58c60496d3a36e69cc43d35cf6830ad9607c030
juan <juancastrillon868@gmail.com>
2025-05-26 15:06:00 -0500

    Reparacion functions asistencia

--- Backend/routes/departamentoroutes.js
+++ Backend/routes/departamentoroutes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const departamentoController = require("../controllers/departamentocontroller");
+const { getGenerativeModel } = require("firebase/ai");
 
 router.get("/", departamentoController.consultarDepartamento);
 router.post("/", departamentoController.editarDepartamento);
