27c222e4988874838e71c2939370c70b32997b5d
juan <juancastrillon868@gmail.com>
2025-05-26 19:30:12 -0500

    cambios departamento

--- netlify/functions/departamento.js
+++ netlify/functions/departamento.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use('/.netlify/functions/departamento', departamentoroutes); // ✅
+app.use('../netlify/functions/departamento', departamentoroutes); // ✅
 
 module.exports.handler = serverless(app);
 
